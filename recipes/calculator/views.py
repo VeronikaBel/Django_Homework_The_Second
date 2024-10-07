c4ab79e2934a49eb69d318f72d73110d0910af09
VeronikaBel <157049993+VeronikaBel@users.noreply.github.com>
2024-10-07 18:03:32 +0200

    Update views.py

--- recipes/calculator/views.py
+++ recipes/calculator/views.py
@@ -1,24 +1,4 @@
 from django.shortcuts import render
-
-DATA = {
-    'omlet': {
-        'яйца, шт': 2,
-        'молоко, л': 0.1,
-        'соль, ч.л.': 0.5,
-    },
-    'pasta': {
-        'макароны, г': 0.3,
-        'сыр, г': 0.05,
-    },
-    'buter': {
-        'хлеб, ломтик': 1,
-        'колбаса, ломтик': 1,
-        'сыр, ломтик': 1,
-        'помидор, ломтик': 1,
-    },
-    # можете добавить свои рецепты ;)
-}
-from django.shortcuts import render
 from django.http import Http404
 
 DATA = {
