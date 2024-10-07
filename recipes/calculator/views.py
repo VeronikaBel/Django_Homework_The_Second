60634159f336aa108cfc0c3c6827a3e37401df5c
Vero_bel <veronika_sia@mail.ru>
2024-10-07 17:54:08 +0200

    Initial commit

---
+++ recipes/calculator/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+from django.shortcuts import render
+from django.http import Http404
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+def view_ingredients( request, dish):
+    if dish not in DATA:
+        raise Http404 (f'Данный рецепт не найден')
+    
+    recipe = DATA[dish]
+    
+    servings = request.GET.get('servings', 1)
+    
+    try:
+        servings= int(servings)
+    except ValueError:
+        servings = 1
+        
+    final_recipe = {ingredient: amount * servings for ingredient, amount in recipe.items()}
+    
+    context = {'recipe': final_recipe}
+    
+    return render (request, 'calculator/index.html', context)
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
